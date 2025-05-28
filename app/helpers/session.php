0ae0194a2f8ba20162969fee35a349400df39ff0
brallacr <brahiam.cruz@gmail.com>
2025-05-27 21:57:25 -0500

    cambios alejandro-code to developer-code v2

--- app/helpers/session.php
+++ app/helpers/session.php
@@ -1,12 +1,7 @@
 <?php
 session_start();
 include_once 'getUrl.php';
-<<<<<<< HEAD
 if (!isset($_SESSION['usuario'])) {
-=======
-
-if (isset($_SESSION['usuario'])) {
->>>>>>> 54726b9 (cambios)
     header("Location: /proyecto_sigia/index.php");
     exit();
 }
