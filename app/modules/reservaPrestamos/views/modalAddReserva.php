2f7ece0662a9861f5b260cf5e7feb3ed5b58cb80
alceronDelgado <luis_aceron@soy.sena.edu.co>
2025-06-17 01:16:51 -0500

    arreglo momentaneo de vista para resolusión de 1366

---
+++ app/modules/reservaPrestamos/views/modalAddReserva.php
@@ -0,0 +1,20 @@
+<!-- Este archivo va a contener una tabla en la que mi idea principal es visualizar los elementos que el usuario instructor puede solicitar. -->
+<div id="modalAddElements" class="modal" style="display: none;">
+    <!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-title">
+            <span id="modalTitle">Elementos disponibles</span>
+            <button type="button" id="closeModalBtn">
+                <span class="close-modal">&times;</span>
+            </button>
+        </div>
+        <!-- Tabla de elementos devolutivos -->
+        <div class="tableElmDevolutivos">
+            <?php include_once 'tablaElmDevolutivos.php'; ?>
+        </div>
+        <!-- Tabla de elementos consumibles -->
+        <div class="tablaElmConsumible">
+            <?php //include_once 'tablaElmConsumible.php'; ?>
+        </div>
+    </div>
+</div>
